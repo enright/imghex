c51dacf663620556323688c6dc59dc92eab33fab
Bill Enright <bill@yisup.com>
2012-06-30 14:55:54 -0700

    cleanup to use as module

--- test.js
+++ test.js
@@ -8,8 +8,7 @@ var express,
 		
 express = require('express');
 jade = require('jade');
-// an ad-hoc minifier created with uglify
-minifier = require('./uglyMinifier').minifier;
+hexMap = require('./lib/imghex');
 
 app = express.createServer();
 
@@ -29,18 +28,6 @@ app.configure(function(){
   app.use('/jade', express.static(__dirname + '/node_modules/jade'));
 });
 
-// these are chosen via environment variable i.e. $ NODE_ENV=production node app.js
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler()); 
-});
-
-// a minified compiled jade template for creating hex maps in the client
-var minifiedMapTemplate = minifier(jade.compile("include hexMapMixin\nmixin hexMap(ranks, files, width, height, defaultImageURL, boardClass, boardRowClass, boardRowEvenClass, boardRowOddClass, boardTileClass,boardFirstTileClass)", { filename: 'views/.', compileDebug: false, client: true }).toString());
-
 app.get('/testMap', function(req, res) {
     res.render('hexMapTest', { layout: false,
 		board: { 
@@ -52,7 +39,7 @@ app.get('/testMap', function(req, res) {
 		    width: 72
 		},
 		// minified jade template for creating client-side maps is available as a local
-		mapTemplate: minifiedMapTemplate
+		mapTemplate: hexMap.clientMapTemplate
 	})
 });
 
