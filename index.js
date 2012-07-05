3cf885854febd4d9cc09fd15d71e408e952f85d6
Bill Enright <bill@yisup.com>
2012-07-04 18:12:36 -0700

    pass in jade compile options, uglify options

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-
-module.exports = require('./lib/imghex');
+// jade options: use the views directory, compile without debug, compile for the client
+// uglify ast_mangle options: mangle top level names
+// uglify ast_squeeze options: defaults
+// uglify gen_code options: defaults
+module.exports = require('./lib/imghex')({ filename: __dirname + '/views/.', compileDebug: false, client: true }, { toplevel: true });
